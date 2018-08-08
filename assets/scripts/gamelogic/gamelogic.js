227cd178d1b58d93f6ff083d9956c4dc894c1d96
jenfox4 <jenniferfox4@gmail.com>
2018-08-08 12:43:29 -0400

    Added user ability to insert O or X
    - Added playerOne whose gamePiece is X
    - Added playerTwo whose gamePiece is O
    - Added function and console log to see playerOne and playerTwo choose
    X or O on the board.

--- assets/scripts/gamelogic/gamelogic.js
+++ assets/scripts/gamelogic/gamelogic.js
@@ -1,10 +1,27 @@
 // game logic for winning/losing/draw
 const gameFunctions = require('./gamelogicfunctions.js')
 
-const gameBoard = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'X', 'X']
+const gameBoard = ['', '', '', '', '', '', '', '', '']
 // 3 by 3 game board of 9 locations
+const playerOne = 'X'
+const playerTwo = 'O'
 
-const xWinningCondition = gameFunctions.checkWinningCondition(gameFunctions.findPositionOfX(gameBoard))
-const oWinningCondition = gameFunctions.checkWinningCondition(gameFunctions.findPositionOfO(gameBoard))
+gameFunctions.play(playerOne, 0, gameBoard)
+console.log(gameBoard)
+gameFunctions.play(playerTwo, 1, gameBoard)
+console.log(gameBoard)
+gameFunctions.play(playerOne, 2, gameBoard)
+console.log(gameBoard)
+gameFunctions.play(playerTwo, 4, gameBoard)
+console.log(gameBoard)
+gameFunctions.play(playerOne, 6, gameBoard)
+console.log(gameBoard)
+gameFunctions.play(playerTwo, 7, gameBoard)
 
-console.log('x', xWinningCondition, 'o', oWinningCondition)
+const xWinningCondition = gameFunctions.checkWinningCondition(gameFunctions.findPosition(gameBoard, playerOne))
+// checks final game status of player x
+
+const oWinningCondition = gameFunctions.checkWinningCondition(gameFunctions.findPosition(gameBoard, playerTwo))
+// checks final game status of player o
+
+console.log('player one', xWinningCondition, 'player two', oWinningCondition)
