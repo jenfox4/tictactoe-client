015ce2065a5b2d75a998a09e900ad0c3ff6ef6e3
jenfox4 <jenniferfox4@gmail.com>
2018-08-08 13:19:47 -0400

    Testing game logic with turns
    - testing each players turn to see if someone has won, if not keep playing.

--- assets/scripts/gamelogic/gamelogic.js
+++ assets/scripts/gamelogic/gamelogic.js
@@ -7,21 +7,18 @@ const playerOne = 'X'
 const playerTwo = 'O'
 
 gameFunctions.play(playerOne, 0, gameBoard)
-console.log(gameBoard)
 gameFunctions.play(playerTwo, 1, gameBoard)
+gameFunctions.play(playerOne, 3, gameBoard)
+gameFunctions.play(playerTwo, 2, gameBoard)
+gameFunctions.play(playerOne, 7, gameBoard)
+gameFunctions.play(playerTwo, 8, gameBoard)
 console.log(gameBoard)
-gameFunctions.play(playerOne, 2, gameBoard)
-console.log(gameBoard)
-gameFunctions.play(playerTwo, 4, gameBoard)
-console.log(gameBoard)
-gameFunctions.play(playerOne, 6, gameBoard)
-console.log(gameBoard)
-gameFunctions.play(playerTwo, 7, gameBoard)
+gameFunctions.minimumPlays(gameBoard)
 
-const xWinningCondition = gameFunctions.checkWinningCondition(gameFunctions.findPosition(gameBoard, playerOne))
+// const xWinningCondition = gameFunctions.checkWinningCondition(gameFunctions.findPosition(gameBoard, playerOne))
 // checks final game status of player x
 
-const oWinningCondition = gameFunctions.checkWinningCondition(gameFunctions.findPosition(gameBoard, playerTwo))
+// const oWinningCondition = gameFunctions.checkWinningCondition(gameFunctions.findPosition(gameBoard, playerTwo))
 // checks final game status of player o
 
-console.log('player one', xWinningCondition, 'player two', oWinningCondition)
+// console.log('player one', xWinningCondition, 'player two', oWinningCondition)
